d13081d97e6e4928ffe5ffd02099d7fa14024ddf
Aaron Honson <honsyb@gmail.com>
2023-03-14 16:16:56 +1030

    Activities added and filter-able

--- src/views/ELC.vue
+++ src/views/ELC.vue
@@ -69,12 +69,13 @@
     class="tileHolder flex gap-4 items-stretch justify-center items-center"
     >
     <div
-        class=""
-        :class="tile.color"
-       :style="`border-color: ${tile.color}; `"
+        class="transition px-2 max-w-[250px] text-center hover:cursor-pointer min-h-[100px] border border-2 "
+        :class=" stageSelected === tile.name ? tile.selected : tile.color"
         v-for="(tile, index) in stages"
         style="line-height: 100px"
+        @click="stageSelected === tile.name ? stageSelected = null : stageSelected = tile.name"
     >
+
         <span
             style="
             display: inline-block;
@@ -93,8 +94,10 @@
     >
       <div
         class="transition px-2 text-center border border-brand-darkblue hover:bg-brand-darkblue hover:text-white hover:cursor-pointer min-h-[100px]"
+        :class="tileSelected === tile.name ? 'bg-brand-darkblue-dark hover:bg-brand-darkblue-dark font-bold text-white' : ''"
         v-for="(tile, index) in tiles"
         style="line-height: 100px"
+        @click="tileSelected === tile.name ? tileSelected = null : tileSelected = tile.name"
       >
         <span
           style="
@@ -105,29 +108,41 @@
           >{{ tile.name }}</span
         >
       </div>
+
     </div>
     <h2>Find a suitable activity</h2>
     <div id="results">
       <div class="filterCont border p-1 grid grid-cols-4 gap-5">
-        <p class="m-2 mb-0" v-for="(filter, idx) in filters">
+        <p class="m-2 mb-0" v-for="(filter, idx) in appliedFilters">
           <small>
-            <select>
-              <option disabled selected>{{ idx }}</option>
-              <option v-for="(option, key) in filter">{{ key }}</option>
+            <strong>{{idx}}</strong>
+            <select v-model="appliedFilters[idx].selected">
+              <option selected></option>
+              <option v-for="(option) in filter.labels" :value="option">{{ option }}</option>
             </select>
           </small>
         </p>
       </div>
-      <p class="mt-4">{{ filterResults() }}</p>
+      <h4>Recommended activities</h4>
+        <TransitionGroup class="resultBox grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4" name="list" tag="div">
+        <a :href="result.link" target="_blank" class="p-4 m-4 shadow-lg border text-center transition hover:shadow-xl hover:-translate-y-1 hover:cursor-pointer" v-for="result in filteredResults">
+          <h5 class="mb-4"><strong>{{result.name}}</strong></h5>
+         <div class="flex flex-wrap"><p class="m-1">Class size:</p><span class="px-4 m-1 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-full" v-for="classSize in result.filters['Class size']"><small>{{classSize}}</small></span></div>
+          <div class="flex flex-wrap"><p class="m-1">Mode:</p><span class="px-4 m-1 bg-blue-200 hover:bg-blue-300 text-blue-800 text-sm font-medium rounded-full" v-for="delivery in result.filters['Mode of delivery']"><small>{{delivery}}</small></span></div>
+          <div class="flex flex-wrap"><p class="m-1">Class time:</p><span class="px-4 m-1 bg-green-200 hover:bg-green-300 text-green-800 text-sm font-medium rounded-full" v-for="time in result.filters['Class time']"><small>{{time}}</small></span></div>
+          <div class="flex flex-wrap"><p class="m-1">Prep time:</p><span class="px-4 m-1 bg-purple-200 hover:bg-purple-300 text-purple-800 text-sm font-medium rounded-full" v-for="time in result.filters['Preparation time']"><small>{{time}}</small></span></div>
+        </a>
+        </TransitionGroup>
+
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import activities from "@/assets/emi_activities.json"
 
 const wrapperClass = "adx max-w-[90%] mx-auto";
-
 
 const slider = {
   "activate": {
@@ -177,7 +192,7 @@ const slider = {
     }
   },
   "check":{
-    "title": "Checking and consolidating understanding",
+    "title": "Checking and consolidating prior knowledge",
     "threshold": 50,
     "link": "",
     color: "#8eccb9"
@@ -208,6 +223,12 @@ const slider = {
   }
 }
 
+const stages = [
+  { name: "Activating prior knowledge", color: "border-chartreuse-light hover:bg-chartreuse-light hover:text-white", selected: "border-chartreuse-light bg-chartreuse-light text-white font-bold" },
+  { name: "Supporting students with subject specific vocabulary",  color: "border-chartreuse-mid hover:bg-chartreuse-mid hover:text-white",  selected: "border-chartreuse-mid bg-chartreuse-mid text-white font-bold" },
+  { name: "Checking and consolidating prior knowledge", color: "border-chartreuse-dark border-2 hover:bg-chartreuse-dark hover:text-white",  selected: "border-chartreuse-dark bg-chartreuse-dark text-white font-bold" },
+  { name: "Summarising content", color: "border-chartreuse-darkest hover:bg-chartreuse-darkest hover:text-white",  selected: " border-chartreuse-darkest bg-chartreuse-darkest text-white font-bold" }
+]
 
 const tiles = [
   { name: "Flipped learning" },
@@ -216,45 +237,110 @@ const tiles = [
   { name: "Translanguaging" },
   { name: "Questioning" },
   { name: "Collaborative activities" },
-  { name: "Discussion-based activities" },
+  { name: "Discussion based activities" },
   { name: "Giving feedback" },
   { name: "Active learning in STEM" }
 ];
 
-const stages = [
-  { name: "Activating prior knowledge", color: "transition px-2 max-w-[250px] text-center hover:cursor-pointer min-h-[100px] border border-2 border-chartreuse-light hover:bg-chartreuse-light hover:text-white" },
-  { name: "Supporting students with subject specific vocabulary",  color: "transition px-2 max-w-[250px] text-center hover:cursor-pointer min-h-[100px] border border-2 border-chartreuse-mid hover:bg-chartreuse-mid hover:text-white" },
-  { name: "Checking and consolidating prior knowledge", color: "transition px-2 max-w-[250px] text-center hover:cursor-pointer min-h-[100px] border border-chartreuse-dark border-2 hover:bg-chartreuse-dark hover:text-white" },
-  { name: "Summarising content", color: "transition px-2 max-w-[250px] text-center hover:cursor-pointer min-h-[100px] border border-2 border-chartreuse-darkest hover:bg-chartreuse-darkest hover:text-white" }
-]
-
-let englishScore = ref(0);
-let filters = ref({
+let appliedFilters = ref({
   "Class size": {
-    "Small": false,
-    "Medium": false,
-    "Large": false,
+    "labels": ["Small","Medium", "Large"],
+    "selected": null
   },
   "Mode of delivery": {
-    "Face-to-face": false,
-    "Online": false,
-    "Both": false,
+    "labels": ["Face-to-face","Online", "Both"],
+    "selected": null
   },
   "Class time": {
-    "<5 minutes": false,
-    "5-10 minutes": false,
-    "10+ minutes": false,
+    "labels": ["<5 minutes","5-10 minutes", "10+ minutes"],
+    "selected": null
   },
   "Preparation time": {
-    "<5 minutes": false,
-    "5-10 minutes": false,
-    "10+ minutes": false,
+    "labels": ["<5 minutes","5-10 minutes", "10+ minutes"],
+    "selected": null
   },
 });
 
-function filterResults() {
-  return "Holding place for results";
-}
+let englishScore = ref(0);
+
+let stageSelected = ref();
+let tileSelected = ref();
+
+
+let filteredStage = computed(() => {
+  return Object.values(activities).filter(activity => {
+    const stage = activity.stage
+    return stage.includes(stageSelected.value)
+  });
+});
+
+let filteredTile = computed(() => {
+  if(stageSelected.value) {
+    return filteredStage.value.filter(activity => {
+      const tile = activity.tiles
+      return tile.includes(tileSelected.value)
+    });
+  } else {
+    return Object.values(activities).filter(activity => {
+      const tile = activity.tiles
+      return tile.includes(tileSelected.value)
+    });
+  }
+});
+
+let filteredResults = computed(() => {
+  if(stageSelected.value && tileSelected.value) {
+    return Object.values(filteredTile.value).filter(activity => {
+      const filters = activity.filters;
+      return Object.keys(appliedFilters.value).every(filterKey => {
+        const selectedValue = appliedFilters.value[filterKey].selected;
+        if (!selectedValue) {
+          return filteredTile.value; // if filter is not selected, don't filter by it
+        }
+        const activityValues = filters[filterKey];
+        return activityValues.includes(selectedValue);
+      });
+    });
+  } else if(stageSelected.value) {
+    return Object.values(filteredStage.value).filter(activity => {
+      const filters = activity.filters;
+      return Object.keys(appliedFilters.value).every(filterKey => {
+        const selectedValue = appliedFilters.value[filterKey].selected;
+        if (!selectedValue) {
+          return filteredStage.value; // if filter is not selected, don't filter by it
+        }
+        const activityValues = filters[filterKey];
+        return activityValues.includes(selectedValue);
+      });
+    });
+  } else if(tileSelected.value) {
+    return Object.values(filteredTile.value).filter(activity => {
+      const filters = activity.filters;
+      return Object.keys(appliedFilters.value).every(filterKey => {
+        const selectedValue = appliedFilters.value[filterKey].selected;
+        if (!selectedValue) {
+          return filteredTile.value; // if filter is not selected, don't filter by it
+        }
+        const activityValues = filters[filterKey];
+        return activityValues.includes(selectedValue);
+      });
+    });
+  } else {
+    return Object.values(activities).filter(activity => {
+      const filters = activity.filters;
+      return Object.keys(appliedFilters.value).every(filterKey => {
+        const selectedValue = appliedFilters.value[filterKey].selected;
+        if (!selectedValue) {
+          return true; // if filter is not selected, don't filter by it
+        }
+        const activityValues = filters[filterKey];
+        return activityValues.includes(selectedValue);
+      });
+    });
+  }
+});
+
+
 </script>
 
 <style>
@@ -263,5 +349,16 @@ h2 {
   line-height: 1.05;
   margin: 2rem 0 0.7rem !important;
   letter-spacing: 0.02rem;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+  transform: translateX(0px);
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
