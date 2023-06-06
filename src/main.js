72128e84ead04f5ffbda478d786c7f9793e21e46
Aaron Honson <honsyb@gmail.com>
2023-06-06 13:04:32 +0930

    width fixes

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
 import 'vue-universal-modal/dist/index.css'
 import VueUniversalModal from 'vue-universal-modal'
+import * as Resize from 'iframe-resizer/js/iframeResizer.contentWindow';
 
 
-createApp(App).use(FloatingVue).use(VueUniversalModal, { teleportTarget: '#modals' }).use(router).mount('#app')
+createApp(App).use(FloatingVue).use(VueUniversalModal, { teleportTarget: '#modals' }).use(router).use(Resize).mount('#app')
 
