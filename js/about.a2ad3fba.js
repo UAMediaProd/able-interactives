029ca79bccd6b44d7eacbe2327bb8b09b6718073
honsaar <honsaar@users.noreply.github.com>
2024-10-27 23:50:36 +0000

    deploy: 8748e9efdbb1a779305d37e7cf3b154221fc372e

---
+++ js/about.a2ad3fba.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkable_interactives"]=self["webpackChunkable_interactives"]||[]).push([[443],{89:function(n,t){t.Z=(n,t)=>{const e=n.__vccOpts||n;for(const[c,r]of t)e[c]=r;return e}},7381:function(n,t,e){e.r(t),e.d(t,{default:function(){return l}});var c=e(3396);const r={class:"about"},s=(0,c._)("h1",null,"This is an about page",-1),a=[s];function u(n,t){return(0,c.wg)(),(0,c.iD)("div",r,a)}var i=e(89);const o={},f=(0,i.Z)(o,[["render",u]]);var l=f}}]);
+//# sourceMappingURL=about.a2ad3fba.js.map
